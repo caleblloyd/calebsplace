f694d4f895309ff5865ebdc5f56248bddfa024a4
Caleb Lloyd <caleblloyd@gmail.com>
2017-04-19 10:41:10 -0400

    remove vuejs ui

--- ui/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.html' {
-    var _: string;
-    export default  _;
-}
--- ui/src/dotvue/DotvueFetch.ts
+++
@@ -1,8 +0,0 @@
-var fetchPolyfill:any
-if (typeof(window) !== 'undefined'){
-    fetchPolyfill = fetch
-} else {
-    fetchPolyfill = require('node-fetch').default
-}
-
-export default fetchPolyfill as (input: RequestInfo, init?: RequestInit) => Promise<Response>
